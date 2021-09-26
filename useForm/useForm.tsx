f72dfd963e4e659ae8fcf09fa3b373e38af3ab4d
Arthy250 <46385075+Arthy250@users.noreply.github.com>
2021-09-26 17:06:42 -0500

    useForm con tipado estricto typescript

---
+++ useForm/useForm.tsx
@@ -0,0 +1,21 @@
+import { useState } from 'react';
+
+export const useForm = <T extends Object>( initState: T ) => {
+    
+    const [state, setState] = useState( initState );
+
+    const onChange = <K extends Object>( value: K, field: keyof T ) => {
+        setState({
+            ...state,
+            [field]: value
+        });
+    }
+
+    return {
+        ...state,
+        formulario: state,
+        onChange,
+    }
+
+}
+
